62d5cbef2c984c8177d4d45a6c08cd47dbdb822e
BuildTools <teaqllabs@gmail.com>
2022-01-22 14:25:00 -0500

    Added command buffer and improved things.

--- src/x86_64/kernel/drivers/keyboard.h
+++ src/x86_64/kernel/drivers/keyboard.h
@@ -33,11 +33,17 @@
 
 #define CHAR_HIGH_LIMIT 35
 #define CHAR_LOW_LIMIT 10
+#define MAX_CHARS CHAR_HIGH_LIMIT - CHAR_LOW_LIMIT
+
+extern uint8_t command_mode;
 
 const uint8_t* const SC_ASCII = "\x00\x1B" "1234567890-=" "\x08"
 "\x00" "qwertyuiop[]" "\x0D\x1D" "asdfghjkl;'`" "\x00" "\\"
 "zxcvbnm,./" "\x00\x00\x00" " ";
 
+
+char commandBuffer[MAX_CHARS + 1];
+uint16_t commandBufferIdx = 0;
 
 void unmask_kb_irq() {
     outportb(PIC1_DATA_PORT, 0xFD);
@@ -56,23 +62,37 @@ __attribute__((interrupt)) void kb_isr(int_frame32_t* frame) {
     uint8_t scancode = inportb(0x60);
     char ch = SC_ASCII[scancode];
     static uint8_t skip = 0;
+    static uint8_t first_fire = 1;
 
-    if (ch >= 'a' && ch <= 'z' && write_kb_chr && cursor_x < CHAR_HIGH_LIMIT) {
-        char str[2] = "\0\0";
-        ch -= 0x20;
-        str[0] = ch;
+    if (first_fire) {
+        first_fire = 0;
+        commandBuffer[MAX_CHARS] = '\0';
+    }
 
-        vga_puts(str, &main_vga, 0);
-        main_vga -= 2;
-        update_cursor(++cursor_x, cursor_y);
-    } else if (scancode == 57 && cursor_x < 35) {
-        update_cursor(++cursor_x, cursor_y);
-        *main_vga = ' ';
-        main_vga += 2;
-    } else if (scancode == 83 && cursor_x > CHAR_LOW_LIMIT) {
-        update_cursor(--cursor_x, cursor_y);
-        main_vga -= 2;
-        *main_vga = ' ';
+    if (command_mode) {
+        if (ch >= 'a' && ch <= 'z' && cursor_x < CHAR_HIGH_LIMIT) {
+            char str[2] = "\0\0";
+            ch -= 0x20;
+            str[0] = ch;
+
+            vga_puts(str, &main_vga, 0);
+            main_vga -= 2;
+            update_cursor(++cursor_x, cursor_y);
+            commandBuffer[commandBufferIdx] = str[0];
+            ++commandBufferIdx;
+        } else if (scancode == 57 && cursor_x < 35) {
+            update_cursor(++cursor_x, cursor_y);
+            *main_vga = ' ';
+            main_vga += 2;
+            commandBuffer[commandBufferIdx] = ' ';
+            ++commandBufferIdx;
+        } else if (scancode == 83 && cursor_x > CHAR_LOW_LIMIT) {
+            update_cursor(--cursor_x, cursor_y);
+            main_vga -= 2;
+            *main_vga = ' ';
+            commandBuffer[commandBufferIdx] = '\0';
+            --commandBufferIdx;
+        }
     }
  
     outportb(0x20, 0x20); 
--- src/x86_64/kernel/kmain.c
+++ src/x86_64/kernel/kmain.c
@@ -25,7 +25,7 @@
 char* main_vga = (char*)0xB8000;
 unsigned int cursor_x = 10;
 unsigned int cursor_y = 2;
-unsigned char write_kb_chr = 1;
+unsigned char command_mode = 1;
 
 #include "drivers/VGA.h"
 #include "interrupts/IDT.h"
