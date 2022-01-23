1233799c05e3d6455b85791eaece2df7f35a0a93
BuildTools <teaqllabs@gmail.com>
2022-01-23 01:56:26 -0500

    FIXED THAT DAMN WEIRD BUG.

--- src/x86_64/kernel/drivers/keyboard.h
+++ src/x86_64/kernel/drivers/keyboard.h
@@ -30,13 +30,13 @@
 #include "VGA.h"
 #include "../interrupts/IDT.h"
 #include "../util/types.h"
+#include "../util/strings.h"
 
 #define CHAR_HIGH_LIMIT 35
 #define CHAR_LOW_LIMIT 10
 #define MAX_CHARS CHAR_HIGH_LIMIT - CHAR_LOW_LIMIT
 
 extern uint8_t command_mode;
-
 const uint8_t* const SC_ASCII = "\x00\x1B" "1234567890-=" "\x08"
 "\x00" "qwertyuiop[]" "\x0D\x1D" "asdfghjkl;'`" "\x00" "\\"
 "zxcvbnm,./" "\x00\x00\x00" " ";
@@ -51,6 +51,18 @@ void unmask_kb_irq() {
 
 uint8_t writeCh = 1;
 char* kb_drvr_vga = (char*)0xB8000;
+
+uint8_t cmd_buf_cmp(char* str) {
+    uint16_t cmdBufIdx = 0;
+
+    for (int i = 0; i < strlen(str); ++i, ++cmdBufIdx) {
+        if (str[i] != commandBuffer[cmdBufIdx]) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
 
 
 __attribute__((interrupt)) void kb_stub_isr(int_frame32_t* frame) {
@@ -79,18 +91,23 @@ __attribute__((interrupt)) void kb_isr_cmd(int_frame32_t* frame) {
         update_cursor(++cursor_x, cursor_y);
         commandBuffer[commandBufferIdx] = str[0];
         ++commandBufferIdx;
-    } else if (scancode == 57 && cursor_x < 35) {
-        update_cursor(++cursor_x, cursor_y);
-        *main_vga = ' ';
-        main_vga += 2;
-        commandBuffer[commandBufferIdx] = ' ';
-        ++commandBufferIdx;
-    } else if (scancode == 83 && cursor_x > CHAR_LOW_LIMIT) {
+    } else if (SC_ASCII[scancode] == '\x08' && cursor_x > CHAR_LOW_LIMIT) {
         update_cursor(--cursor_x, cursor_y);
         main_vga -= 2;
+        *main_vga = ' '; 
+         --commandBufferIdx;
+        commandBuffer[commandBufferIdx] = ' '; 
+
+    } else if (scancode == 57 && cursor_x < CHAR_HIGH_LIMIT) {
+        update_cursor(++cursor_x, cursor_y);
+        main_vga += 2;
         *main_vga = ' ';
-        commandBuffer[commandBufferIdx] = '\0';
+        commandBuffer[commandBufferIdx] = ' ';
         --commandBufferIdx;
+    } else if (scancode == 28) {
+        if (cmd_buf_cmp("REBOOT")) {
+            __asm__ __volatile__("mov $0x0, %eax; int $0x79");
+        }
     }
  
     outportb(0x20, 0x20); 
